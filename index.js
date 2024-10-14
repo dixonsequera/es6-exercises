c2d0c5101572158a60dfe58aa26adbe2f633f713
Girgetto <girgetto@users.noreply.github.com>
2024-10-14 17:18:20 +0200

    Refactor code and add missing function implementations

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const rectangleArea = (largo, ancho) => largo * ancho;
+const rectangleArea = /* Añade tu código aquí */;
 
-const greet = (nombre) => `Hola, ${nombre}!`;
+const greet = (nombre) => /* Añade tu código aquí */;
 
-const mergeArray = (arr1, arr2) => [...arr1, ...arr2];
+const mergeArray = (arr1, arr2) => /* Añade tu código aquí */;
 
-const ducplicateAndSum = (a, b, ...numeros) => {
+const ducplicateAndSum = (a, b, /* Añade tu código aquí */) => {
   const suma = a + b;
   return numeros.map((numero) => numero + suma);
 };
 
 const setExercise = () => {
-  const miSet = new Set([1, 2, 3, 3, 4, 5]);
-  return miSet.size;
+  /* Añade tu código aquí */
 };
 
 const miMapa = new Map();
@@ -20,26 +19,9 @@ miMapa.set("edad", 30);
 miMapa.set("profesion", "Desarrollador");
 
 const mapExercise = () => {
-  miMapa.set("profesion", "Ingeniero de Software");
-  miMapa.delete("edad");
-
-  return miMapa;
+  /* Añade tu código aquí */
 };
 
 const countRepeated = (string) => {
-  const count = new Map();
-
-  for (let i = 0; i < string.length; i++) {
-    const item = string[i];
-    let counter = 0;
-
-    if (count.has(item)) {
-      counter = count.get(item) + 1
-    } else {
-      counter = 1;
-    }
-
-    count.set(item, counter);
-  }
-  return count;
+  /* Añade tu código aquí */
 };
