e022569f217a0c30a52c53ee39bf51b4d97b8d91
dixonsequera <104364319+dixonsequera@users.noreply.github.com>
2024-10-19 14:04:55 +0200

    TAREA

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const rectangleArea = /* Añade tu código aquí */;
+const rectangleArea = (largo, ancho) => largo * ancho;
 
-const greet = (nombre) => /* Añade tu código aquí */;
+const greet = (nombre) => `Hola, ${nombre}!`;
 
-const mergeArray = (arr1, arr2) => /* Añade tu código aquí */;
+const mergeArray = (arr1, arr2) => [...arr1, ...arr2];
 
-const ducplicateAndSum = (a, b, /* Añade tu código aquí */) => {
+const ducplicateAndSum = (a, b, ...numeros) => {
   const suma = a + b;
   return numeros.map((numero) => numero + suma);
 };
 
 const setExercise = () => {
-  /* Añade tu código aquí */
+  const miSet = new Set([1, 2, 3, 3, 4, 5]);
+  return miSet.size;
 };
 
 const miMapa = new Map();
 miMapa.set("nombre", "Juan");
-miMapa.set("edad", 30);
+miMapa.set("edad", 45);
 miMapa.set("profesion", "Desarrollador");
 
 const mapExercise = () => {
-  /* Añade tu código aquí */
+  miMapa.set("profesion", "Ingeniero de Software");
+  miMapa.delete("edad");
+
+  return miMapa;
 };
 
 const countRepeated = (string) => {
-  /* Añade tu código aquí */
+  const count = new Map();
+
+  for (let i = 0; i < string.length; i++) {
+    const item = string[i];
+    let counter = 0;
+
+    if (count.has(item)) {
+      counter = count.get(item) + 1
+    } else {
+      counter = 1;
+    }
+
+    count.set(item, counter);
+  }
+  return count;
 };
+
